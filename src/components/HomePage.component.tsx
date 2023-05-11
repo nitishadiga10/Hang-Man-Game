adf8a8ef9d3254b0f409136cf78846aa630c01c9
nitishadiga10 <nitish.adiga10@gmail.com>
2023-05-12 03:41:15 +0530

    finalizing the proper flow

--- src/components/HomePage.component.tsx
+++ src/components/HomePage.component.tsx
@@ -2,47 +2,53 @@ import { useEffect, useState } from 'react';
 import { Hangman } from "./HangMan.component"
 import { InputText } from "./InputText.component"
 import { KeyBoard } from "./KeyBoard.component"
+import wordsRepo from '../questionWord.json';
 
+const getTheWord = () => {
+    return wordsRepo[Math.floor(Math.random() * wordsRepo.length)]
+}
 
 export const HomePage = () => {
 
-    const [latestWord, setLatestWord] = useState<string[]>("ANUSHA".split(""));
-    const [attempts, setAttempts] = useState<number>(6);
+    const [latestWord, setLatestWord] = useState<string[]>(getTheWord().split(""));
+    const [wrongAttempts, setWrongAttempts] = useState<number>(0);
     const [typedLetters, setTypedLetters] = useState<string[]>([]);
     const [result, setResult] = useState<string>("");
 
     useEffect(() => {
+    let wrongLetters = 0;
+        typedLetters.forEach(letter => {
+            if (!latestWord.includes(letter)) wrongLetters += 1;
+        });
+        setWrongAttempts(wrongLetters);
+
         const tempData = latestWord.every(el => typedLetters.includes(el));
-        if (attempts === 0 && !tempData) {
+        if (wrongLetters === 6 && !tempData) {
             setResult("LOST");
         } else if (tempData) setResult("WON");
-    }, [attempts]);
+
+    }, [typedLetters.length]);
 
     const handleKeyBoardClick = (e: any) => {
-        console.log(e.target.innerHTML);
-        setAttempts(attempts => attempts - 1);
         setTypedLetters(prevState => [...prevState, e.target.innerHTML]);
     }
 
     const handleRefresh = () => {
-        setAttempts(6);
-        setResult("");
-        setTypedLetters([]);
+        window.location.reload();
     }
 
     return <div className="flex-col flex  justify-center items-center h-full w-full gap-8">
         {!result && <h1 className='mt-5 font-bold text-[30px]'>
-            Hello There, Guess the word, You Got only <span className='text-[red]'>{` ${attempts} `}</span> 
-            chances to Hang the Man!</h1>}
+            Hello There ! Guess the word and Save the Man!</h1>}
             {!!result && <div className='flex flex-row justify-center items-center mt-5'>
-            <h1 className='font-bold text-[30px]'>{result === "WON" ? "You Hanged the Man ! Congrats !!! " : 
-            "You Couldn't Hang the Man, Oops!!!!"}</h1>
+            <h1 className='font-bold text-[30px]'>{result === "WON" ? "You Saved the Man ! Bravo !!! " : 
+            "You Hanged the Man, Oops!!!!"}</h1>
             <button className='px-5 bg-sky-500 rounded ml-5 h-[40px] text-xl
             hover:bg-white hover:border-black hover:border-2' onClick={handleRefresh}
             >Play Again !</button>
             </div>}
             {}
-        <Hangman />
+        <Hangman wrongAttempts = {wrongAttempts}/>
         <InputText latestWord={latestWord} typedLetters={typedLetters} result = {result} />
         <KeyBoard 
         handleKeyBoardClick={handleKeyBoardClick} 
